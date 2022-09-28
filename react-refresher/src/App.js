7200ecf0dd2f0b6df1ad99f506ce86ef9a4e1b8c
gyenattila <gyenattila@gmail.com>
2022-09-28 22:54:54 +0200

    cleanup files, working w/ multiple components

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import GoalList from './components/GoalList';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='course-goals'>
+      <h2>Goals</h2>
+      <GoalList />
     </div>
   );
+};
+
+/**
+ * Class based
+ */
+/**
+class App extends React.Component {
+  render() {
+    return <h1>Hello React</h1>
+  }
 }
+ */
 
 export default App;
---
+++ react-refresher/src/components/GoalList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './GoalList.css';
+
+const GoalList = () => {
+  return (
+    <ul className='goal-list'>
+      <li>Finish</li>
+      <li>All about React</li>
+      <li>Help others</li>
+    </ul>
+  );
+};
+
+export default GoalList;
