a536d373e14fff4bce521acbb355f63a1e8672fc
gyenattila <gyenattila@gmail.com>
2022-09-29 07:23:15 +0200

    adding a UsersList page / Component

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h1>Users works</h1>;
+  const USERS = [
+    {
+      id: Math.random.toString(),
+      image: 'Jack',
+      name: 'https://pcdn.hu/articles/images-xl/o/l/a/olaszorszag-firenze-360624.jpg',
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
