b2c5fa58f5032c77b59c054e86a6d12fc42cfad9
gyenattila <gyenattila@gmail.com>
2022-09-29 06:32:41 +0200

    fetch user input, two-way binding

--- react-refresher/src/components/NewGoal/NewGoal.js
+++ react-refresher/src/components/NewGoal/NewGoal.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewGoal.css';
 
 const NewGoal = props => {
   const { onAddNewGoal } = props;
+  const [enteredText, setEnteredText] = useState('');
+
   const addGoalHandler = event => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'New goal',
+      text: enteredText,
     };
 
+    setEnteredText('');
     onAddNewGoal(newGoal);
+  };
+
+  const textChangeHandler = event => {
+    setEnteredText(event.target.value);
   };
 
   return (
     <form className='new-goal' onSubmit={addGoalHandler}>
-      <input type='text' />
+      <input type='text' value={enteredText} onChange={textChangeHandler} />
       <button type='submit'>Add Goal</button>
     </form>
   );
