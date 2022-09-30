3014c53ff7561d602c9407edc905feef95c1561d
gyenattila <gyenattila@gmail.com>
2022-09-30 14:04:15 +0200

    add bodyParser to application, use nodemon to auto refresh the backend

--- node-js-express-refresher/app.js
+++ node-js-express-refresher/app.js
@@ -1,31 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  let body = '';
+// app.use((req, res, next) => {
+//   let body = '';
 
-  req.on('data', chunk => {
-    body += chunk;
-  });
+//   req.on('data', chunk => {
+//     body += chunk;
+//   });
 
-  req.on('end', () => {
-    const userName = body.split('=')[1];
+//   req.on('end', () => {
+//     const userName = body.split('=')[1];
 
-    if (userName) {
-      req.body = { name: userName };
-    }
-    next();
-  });
+//     if (userName) {
+//       req.body = { name: userName };
+//     }
+//     next();
+//   });
+// });
+
+/**
+ * Use bodyParser to parse the incoming data.
+ */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/user', (req, res, next) => {
+  res.send(req.body.username);
 });
 
-app.use((req, res, next) => {
-  if (req.body) {
-    return res.send(req.body.name);
-  }
-
+app.get('/', (req, res, next) => {
   res.send(`
-    <form method="POST">
+    <form action="/user" method="POST">
       <input type="text" name="username">
       <button type="submit"> Create </button>
     </form>
