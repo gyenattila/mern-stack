5cecc7f6ddd905608c7bd5bf12ab063cd740d18a
gyenattila <gyenattila@gmail.com>
2022-10-03 06:58:13 +0200

    adding place-specific routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,32 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in places');
-  res.json({ message: 'It works' });
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire state',
+    description: 'One of the most famous sky scraper',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg',
+    address: '20 W 34th St., New York, NY 10001',
+    creatorId: 'u1',
+    location: {
+      lat: 40.7484,
+      lng: -73.9878,
+    },
+  },
+];
+
+router.get('/:placeId', (req, res, next) => {
+  const placeId = req.params.placeId;
+  const place = DUMMY_PLACES.find(place => place.id === placeId);
+  res.json({ place });
+});
+
+router.get('/user/:userId', (req, res, next) => {
+  const userId = req.params.userId;
+  const place = DUMMY_PLACES.find(place => place.creatorId === userId);
+  res.json({ place });
 });
 
 module.exports = router;
